1cef4897e486cc9277f97c5987048121ce47c3a8
OvchinnikovKirill <Ovchinnikovk758@gmail.com>
2021-05-02 11:31:29 +0300

    calculator

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
+import calculator from './modules/calculator';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -16,4 +17,5 @@ window.addEventListener('DOMContentLoaded', () => {
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
     showMoreStyles('.button-styles', '#styles .row');
+    calculator('#size', '#material', '#options', '.promocode', '.calc-price');
 });
